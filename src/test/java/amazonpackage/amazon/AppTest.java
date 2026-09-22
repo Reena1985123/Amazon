ea6e90be3d4cdf9fc53325aada30b1861f0a2443
Reena <rina192001@gmail.com>
2026-09-22 09:14:30 -0400

    first commit

---
+++ src/test/java/amazonpackage/amazon/AppTest.java
@@ -0,0 +1,14 @@
+package amazonpackage.amazon;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    
+{
+    WebDriver driver=new ChromeDriver();
+    
